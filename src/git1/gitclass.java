62f702e10bf834912f159411d2a9bb189689f72d
Computer World <Computer World@DESKTOP-NDK2DJM>
2020-11-25 13:40:06 +0530

    first_cummite....

---
+++ src/git1/gitclass.java
@@ -0,0 +1,18 @@
+package git1;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		
+		String hunger= "yes";
+		
+		if(hunger== "yes")
+		{
+			System.out.println("eat something");
+		}
+		else
+			{
+			System.out.println("Do work");
+            }
+	}
+}
